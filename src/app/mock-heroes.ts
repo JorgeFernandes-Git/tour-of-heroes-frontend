db983c6dad043641d93727699b816623d10bffbf
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-11-12 19:50:20 +0000

    Add on select.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: "Wolverine"},
+  {id: 2, name: "Spider Man"},
+  {id: 3, name: "Batman"},
+  {id: 4, name: "Hulk"},
+  {id: 5, name: "Wonder Woman"},
+  {id: 6, name: "Iron Man"},
+  {id: 7, name: "Caption America"},
+  {id: 8, name: "Cat Woman"},
+];
+
